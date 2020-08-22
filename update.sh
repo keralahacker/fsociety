ac88e382a60afd7cd7a978300a43bad1fdd3cefb
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-08-22 17:17:52 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-git clone --depth=1 https://github.com/Manisso/fsociety.git
+git clone --depth=1 https://github.com/keralahacker/fsociety.git
 sudo chmod +x fsociety/install.sh
 bash fsociety/install.sh
