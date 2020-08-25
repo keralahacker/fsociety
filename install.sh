08855252d286a7e76d8aa7ac4e2312bbcad2be17
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-08-25 23:20:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ fi
 
 echo "[✔] Installing ...";
 echo "";
-git clone --depth=1 https://github.com/Manisso/fsociety "$INSTALL_DIR";
+git clone --depth=1 https://github.com/keralahacker/fsociety "$INSTALL_DIR";
 echo "#!$BASH_PATH
 python $INSTALL_DIR/fsociety.py" '${1+"$@"}' > "$INSTALL_DIR/fsociety";
 chmod +x "$INSTALL_DIR/fsociety";
